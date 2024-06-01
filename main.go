946706c4351c9bf46dfe4caff95ac79792f0857b
21-wm <15770981792@163.com>
2024-06-02 00:39:38 +0800

    算法函数调用

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ import (
 )
 
 func main() {
+	// z变换
 	test := likou.Convert("PAYPALISHIRING", 3)
 	fmt.Println(test)
+
+	// 寻找两个有序数组的中位数
+	nums1 := []int{1, 2, 3}
+	nums2 := []int{4, 5, 6, 7}
+	index := likou.FindMedianSortedArrays(nums1, nums2)
+	fmt.Println(index)
 }
